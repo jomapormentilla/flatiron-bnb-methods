8da3d629ba468b0b8c25bddd640409a09821389c
jomapormentilla <jomapormentilla@gmail.com>
2021-01-04 21:33:00 -0500

    commit tree cefd067b6ab9ba54246c1c8e08560ed30bc8bc5c

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,37 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, through: :listings
 
+  def city_openings( date_start, date_end )
+    # @listings = Listing.all.where()
+  end
+
+  def self.highest_ratio_res_to_listings
+    compare = 0
+    result = nil
+    City.all.includes(:reservations).each do |city|
+      if city.listings.size > 0 && city.reservations.size > 0
+        ratio = city.reservations.size / city.listings.size
+        if ratio > compare
+          compare = ratio
+          result = city
+        end
+      end
+    end
+    result
+  end
+
+  def self.most_res
+    compare = 0
+    result = nil
+    City.all.includes(:reservations).each do |city|
+      if city.reservations.size > 0 && city.reservations.size > compare
+        compare = city.reservations.size
+        result = city
+      end
+    end
+    result
+  end
 end
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,19 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
   
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  # validates :neighborhodd, presence: true
+
+  after_create :change_host_status
+
+  private
+
+  def change_host_status
+    self.host.host = !self.host.host
+    binding.pry
+  end
 end
